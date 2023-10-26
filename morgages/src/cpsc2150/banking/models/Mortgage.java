de673159af1ce5a3129fb307223cbd6e7bda7d69
ghilles23 <ghilles@clemson.edu>
2023-10-26 10:51:28 -0400

    Mortgage.java changes Co-authored-by: bjmolin <bjmolin@users.noreply.github.com>

--- morgages/src/cpsc2150/banking/models/Mortgage.java
+++ morgages/src/cpsc2150/banking/models/Mortgage.java
@@ -1,5 +1,41 @@
 package cpsc2150.banking.models;
 
-public class Mortgage {
+/*
+ * @invariant 
+ */
+
+
+public class Mortgage extends AbsMortgage implements IMortgage {
+    double houseCost;
+    double downPayment;
+    int years;
+    ICustomer customer;
+    public Mortgage(double hCost, double dPayment, int yrs, ICustomer cx) {
+        hCost = houseCost;
+        dPayment = downPayment;
+        years = yrs;
+        customer = cx;
+    }
+    
+    boolean loanApproved(){
+        
+    }
+    
+
+    double getPayment(){
+
+    }
+
+    double getRate(){
+        
+    }
+
+    double getPrincipal(){
+        return houseCost - downPayment;
+    }
+
+    int getYears(){
+        return years;
+    }
     
 }
