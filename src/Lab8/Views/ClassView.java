33e529021b1eda03b85fc1186aab8d6b5c7d6479
ghilles23 <ghilles@clemson.edu>
2023-11-27 12:46:20 -0500

    removed space for autograder

--- src/Lab8/Views/ClassView.java
+++ src/Lab8/Views/ClassView.java
@@ -28,7 +28,7 @@ public class ClassView
 	 */
 	public void printClassInformation(String aClassName, int aClassNumber, String aClassDPT)
 	{	
-		System.out.println("Class Details: ");
+		System.out.println("Class Details:");
 		System.out.println("Name: "+ aClassName);
 		System.out.println("Class Number: "+ aClassNumber);
 		System.out.println("Class Department: "+ aClassDPT);
